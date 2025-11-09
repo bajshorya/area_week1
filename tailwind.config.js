494d267ef0ec05efae33d365da58b89aab5618b2
SHORYA BAJ <jain.shorya11@gmail.com>
2025-11-09 11:36:12 +0530

    fix : mobile nav & trustedBy section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      tablet: "800px",
+      desktop: "1280px",
+    },
+    color: {
+      primary: "",
+    },
+    fontFamily: {
+      "dm-sans": ['"DM Sans"', "sans-serif"],
+      "crimson-text": ['"Crimson Text"', "serif"],
+    },
   },
   plugins: [],
 };
